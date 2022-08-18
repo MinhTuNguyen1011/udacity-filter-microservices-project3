d9fff0a14fa1e25bf8debf7e2c6cd24c9e4a4fd2
MinhTuNguyen1011 <tunm62@wru.vn>
2022-08-18 13:31:16 +0700

    project 3
    update project

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://acaf7440b644749d29754fef3816d6d4-1837055132.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
